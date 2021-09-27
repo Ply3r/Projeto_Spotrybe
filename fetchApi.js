acb2d2c0d49990aee5c36b4c0cfdda75f6c4fe3a
Leandro <play3rtox1c@gmail.com>
2021-09-27 19:05:23 -0300

    a

--- fetchApi.js
+++ fetchApi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 fetch("https://api.spotify.com/v1/search?q=twenty%20one%20pilots&type=track%2Cartist&market=US&limit=10&offset=5", {
   headers: {
     Accept: "application/json",
-    Authorization: "Bearer BQCSOJGqbarZQ3ATlQTkocU6cyJ-FOb2hKbYnxTF4cvZQHWWHKYyWKK-GyAIWJW7t5NF-Kp7orqS54aoppwgmkrImIzoJ_xjKne7efLKPsYyoIkNGfw18DKyI4UNvMVpw1aDb0KYBKNeCo8hOziSbiKh7-EATv0",
+    Authorization: "Bearer BQCD_L5kH2zRogqyRyQdW704jMCJ_bQR-eXsMQ0m3j1UGzRRJVkC5SfaBB2km1aLsBVnUweqqWC9FhcZHUG7h-HVhEHl8FkuzZZo_qvfryrWTlcdsNulk8GPAotFo-4YRsQHZztl52GKRN2ffMmn8VrK1rl2J3E",
     "Content-Type": "application/json"
   }
 })
