f2e9a70dc5d071c8cdced5d4851d84cf80a0fa58
Effolex <Effolex@gmail.com>
2021-09-27 21:51:36 -0300

    atualização do spotify
    fetch agora é parte do elemento spotify

--- spotify.js
+++ spotify.js
@@ -1,10 +1,32 @@
 export default class spotify {
+  token = ''
 
-  static async getTrack(id, token) {
+  constructor() {
+    this.token = this.getToken();
+  }
+  async getToken() {
+    const CLIENT_ID = '062ce822e4104fa4827a8db0ee93263d';
+    const CLIENT_SECRET = 'e2e8aa8221984bb9959a7e2ef62de1e1';
+    const API_TOKEN = 'https://accounts.spotify.com/api/token';
+    const response = await fetch(API_TOKEN, {
+      body: 'grant_type=client_credentials',
+      headers: {
+        Authorization: 'Basic '+ btoa(`${CLIENT_ID}:${CLIENT_SECRET}`),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method : 'POST',
+    })
+    if (response.status == 200) {
+      let data = await response.json();
+      return data.access_token;
+    }
+  }
+
+  async getTrack(id) {
     const result = await fetch(`https://api.spotify.com/v1/tracks/${id}?market=BR`, {
       headers: {
       Accept: "application/json",
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${this.token}`,
       "Content-Type": "application/json"
       }
     })
@@ -14,22 +36,17 @@ export default class spotify {
     return data;
   }
 
-  static async getNPossibleTracks(name = '', possibleTracks, token) {
+  async getNPossibleTracks(name = '', possibleTracks) {
     const formatedName = name.replaceAll(' ', '%20');
     const response = await fetch(`https://api.spotify.com/v1/search?q=${formatedName}&type=track&market=BR&market=US&limit=${possibleTracks}`, 
     {
       headers: {
         Accept: "application/json",
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${this.token}`,
         "Content-Type": "application/json"
       }
     })
     console.log(response);
-    console.log({
-      Accept: "application/json",
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json"
-    });
     const data = await response.json();
     return data;
   }
