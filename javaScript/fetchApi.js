813bbe6dbf6ff772d8f91a7f733c4244e4ffc2e0
Effolex <Effolex@gmail.com>
2021-09-28 14:26:42 -0300

    Update fetchApi.js

--- javaScript/fetchApi.js
+++ javaScript/fetchApi.js
@@ -6,11 +6,12 @@ window.onload = async () => {
   const spotTrybe = await createAsyncSpotTrybe();
   const track = await spotTrybe.getNPossibleTracks('Blinding Lights', 1);
   const trackId = track.tracks.items[0].id;
-  spotTrybe.getTrackById(trackId);
-  spotTrybe.getUserPlaylists('12147540058',5);
-  spotTrybe.getPlaylist('2G73gq2YWPWwToeAwNaD2k');
-  spotTrybe.getListOfBrowseCategories(5);
-  spotTrybe.getListOfNewReleases(5);
-  spotTrybe.getCategorysPlaylists('rock',5);
-  spotTrybe.getListOfFeaturedPlaylists(5);
+  await spotTrybe.getTrackById(trackId);
+  await spotTrybe.getUserPlaylists('12147540058',5);
+  await spotTrybe.getPlaylist('2G73gq2YWPWwToeAwNaD2k');
+  await spotTrybe.getListOfBrowseCategories(5);
+  await spotTrybe.getListOfNewReleases(5);
+  await spotTrybe.getCategorysPlaylists('rock',5);
+  await spotTrybe.getListOfFeaturedPlaylists(5);
+  await spotTrybe.getUserProfileInfo('12147540058');
 }
