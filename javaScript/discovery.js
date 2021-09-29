2103d5654ec6531962d3953b3589e0e5f6b4e891
Leandro <play3rtox1c@gmail.com>
2021-09-28 21:05:09 -0300

    adiciona discovery

---
+++ javaScript/discovery.js
@@ -0,0 +1,75 @@
+import createAsyncSpotTrybe from "./spotify.js";
+
+const getCategories = async () => {
+  const spotTrybe = await createAsyncSpotTrybe();
+  let { categories:{items} } = await spotTrybe.getListOfBrowseCategories(20)
+  createAndAppendCategories(items)
+}
+
+const getPlaylist = async (id) => {
+  const spotTrybe = await createAsyncSpotTrybe();
+  const container = document.querySelector('.grid-container')
+  let { playlists: { items } } = await spotTrybe.getCategorysPlaylists(id, 20);
+  console.log(items)
+  items.forEach(({name, id, images}) => {
+    const div = createDiv(id,'grid-item');
+    div.addEventListener('click', () => {
+      cleanContentAndGetPlaylist(id);
+    })
+    appendToElement(div,[createImage(images[0].url),createName(name)]);
+    container.append(div)
+  });
+} 
+
+function createAndAppendCategories(array) {
+  const container = document.querySelector('.grid-container')
+  array.forEach(({ name, id, icons }) => {
+    const { url } = icons[0];
+    const div = createDiv(id,'grid-item');
+    div.addEventListener('click', () => {
+      cleanContentAndGetPlaylist(id);
+    })
+    appendToElement(div,[createImage(url),createName(name)]);
+    container.append(div)
+  })
+}
+
+function createDiv(id, classe) {
+  const div = document.createElement('div');
+  div.id = id;
+  div.className = classe;
+  return div
+}
+
+function appendToElement(div,listOfElements) {
+  listOfElements.forEach((element) => div.append(element));
+}
+
+function createImage(url) {
+  const image = document.createElement('img');
+  image.src = url;
+  return image;
+}
+
+function createName(name) {
+  const h2 = document.createElement('h2')
+  h2.innerText = name;
+  return h2;
+}
+
+function createGeneric(textContent, elementToCreate) {
+  const generic = document.createElement(elementToCreate)
+  generic.innerText = textContent;
+  return generic;
+}
+
+function cleanContentAndGetPlaylist(id) {
+  const container = document.querySelector('.grid-container');
+  container.innerHTML = '';
+  console.log(id);
+  getPlaylist(id);
+}
+
+window.onload = () => {
+  getCategories()
+}
--- javaScript/fetchApi.js
+++ javaScript/fetchApi.js
@@ -4,21 +4,7 @@ import createAsyncSpotTrybe from "./spotify.js";
 window.onload = async () => {
   // precisa de esperar inicializar 
   const spotTrybe = await createAsyncSpotTrybe();
-  const track = await spotTrybe.getNPossibleTracks('Blinding Lights', 1);
-  const trackId = track.tracks.items[0].id;
-  await spotTrybe.getTrackById(trackId);
-  await spotTrybe.getUserPlaylists('12147540058',5);
-  console.log('playlist')
-  console.log(await spotTrybe.getPlaylist('2G73gq2YWPWwToeAwNaD2k'))
-  console.log('fim playlist')
-  await spotTrybe.getListOfBrowseCategories(5);
-  await spotTrybe.getListOfNewReleases(5);
-  await spotTrybe.getCategorysPlaylists('rock',5);
-  console.log('aaaa');
-  await spotTrybe.getListOfFeaturedPlaylists(1);
-  await spotTrybe.getUserProfileInfo('12147540058');
-  console.log('tracks');
-  await spotTrybe.getSeveralTracksById('7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ,2takcwOaAZWiXQijPHIx7B')
+  console.log(await spotTrybe.getListOfBrowseCategories(5));
 }
 
 'https://api.spotify.com/v1/tracks?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B&market=ES'
