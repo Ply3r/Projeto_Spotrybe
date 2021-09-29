c0b611fd662bd9c443b88bf1b78bfc706602cf57
Effolex <Effolex@gmail.com>
2021-09-28 21:56:03 -0300

    Update fetchApi.js

--- javaScript/fetchApi.js
+++ javaScript/fetchApi.js
@@ -5,6 +5,7 @@ window.onload = async () => {
   // precisa de esperar inicializar 
   const spotTrybe = await createAsyncSpotTrybe();
   console.log(await spotTrybe.getListOfBrowseCategories(5));
+  console.log(await spotTrybe.getTrackById('4DWKtQVURjepn8uzBgmwuv'));
 }
 
 'https://api.spotify.com/v1/tracks?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B&market=ES'
