6eb4cad3af35c69efcef1da1f6bea28da224cb0d
Effolex <Effolex@gmail.com>
2021-09-28 21:08:41 -0300

    Update fetchApi.js

--- javaScript/fetchApi.js
+++ javaScript/fetchApi.js
@@ -4,20 +4,8 @@ import createAsyncSpotTrybe from "./spotify.js";
 window.onload = async () => {
   // precisa de esperar inicializar 
   const spotTrybe = await createAsyncSpotTrybe();
-  const track = await spotTrybe.getNPossibleTracks('Blinding Lights', 1);
-  const trackId = track.tracks.items[0].id;
-  await spotTrybe.getTrackById(trackId);
-  await spotTrybe.getUserPlaylists('12147540058',5);
-  console.log(await spotTrybe.getPlaylist('2G73gq2YWPWwToeAwNaD2k'))
-  console.log('playlist')
-  await spotTrybe.getListOfBrowseCategories(5);
-  console.log('fim playlist')
-  await spotTrybe.getCategorysPlaylists('brazil',5)
-  await spotTrybe.getListOfNewReleases(5);
-  await spotTrybe.getListOfFeaturedPlaylists(1);
-  await spotTrybe.getUserProfileInfo('12147540058');
-  console.log('tracks');
-  await spotTrybe.getSeveralTracksById('7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ,2takcwOaAZWiXQijPHIx7B')
+  await spotTrybe.getCategorysPlaylists(5);
+  
 }
 
 'https://api.spotify.com/v1/tracks?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B&market=ES'
