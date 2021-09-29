839ab5cd53356a3633d4ccf2e8477dfee78a60e0
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-09-29 12:54:14 -0300

    Removidos bugs do player

--- javaScript/player.js
+++ javaScript/player.js
@@ -1,45 +1,70 @@
+const addDuration = (duration) => {
+  const maxTimeSpan = document.querySelector('.player-max-time');
+
+  maxTimeSpan.innerText = `00:${duration}`;
+};
+
 const createAudioElement = (link) => {
   const prevAudio = document.getElementById('playerMusic');
-  const maxTimeSpan = document.querySelector('.player-max-time');
   if (prevAudio) prevAudio.remove();
 
   const audio = new Audio(link);
   audio.id = 'playerMusic';
   audio.addEventListener('loadeddata', () => {
-    maxTimeSpan.innerText = `00:${parseInt(audio.duration)}`;
+    addDuration(parseInt(audio.duration));
   });
 
   document.body.appendChild(audio);
 };
 
-const getCurrentTime = (music) => music.currentTime.toString().padStart(2, '0');
+const getCurrentTime = (music) => parseInt(music.currentTime).toString().padStart(2, '0');
+
+const getCurrentTimeFloat = (music) => music.currentTime;
+
+const handleProgressBar = (time) => {
+  const progressBar = document.querySelector('.progress-bar');
+
+  progressBar.ariaValueNow = time;
+  progressBar.style.width = `${time * (100 / 30)}%`;
+};
+
+const handlePlayButton = (isPlaying) => {
+  if (isPlaying) {
+    playBtn.classList.add('d-none');
+    pauseBtn.classList.remove('d-none');
+  } else {
+    playBtn.classList.remove('d-none');
+    pauseBtn.classList.add('d-none');
+  }
+};
 
 const playerProgressHandler = (currentMusic) => {
   const currentTimeSpan = document.querySelector('.player-current-time');
-  const progressBar = document.querySelector('.progress-bar');
   const currentTimeByMusic = getCurrentTime(currentMusic);
 
   currentTimeSpan.innerText = `00:${currentTimeByMusic}`;
-  progressBar.ariaValueNow = currentTimeByMusic;
-  progressBar.style.width = `${currentTimeByMusic * 3.3}%`;
+  handleProgressBar(getCurrentTimeFloat(currentMusic));
 
-  const plusOneSec = setInterval(async () => {
-    if (!currentMusic.paused) {
-      const currentTimeArr = currentTimeSpan.innerText.split('');
-      if (currentTimeArr[4] === '9') {
-        currentTimeArr[4] = 0;
-        currentTimeArr[3] = Number(currentTimeArr[3]) + 1;
-      } else {
-        currentTimeArr[4] = Number(currentTimeArr[4]) + 1;
-      }
-      progressBar.ariaValueNow = Number(currentTimeArr.slice(-2).join('')) * 3.3;
-      progressBar.style.width = `${Number(currentTimeArr.slice(-2).join('')) * 3.3}%`;
-      currentTimeSpan.innerText = currentTimeArr.join('');
+  const plusOneSec = setInterval(() => {
+    const currentTimeArr = currentTimeSpan.innerText.split('');
+    if (currentTimeArr[4] === '9') {
+      currentTimeArr[4] = 0;
+      currentTimeArr[3] = Number(currentTimeArr[3]) + 1;
+    } else {
+      currentTimeArr[4] = Number(currentTimeArr[4]) + 1;
     }
+    currentTimeSpan.innerText = currentTimeArr.join('');
   }, 1000);
+
+  const plusFiftyMSec = setInterval(() => {
+    handleProgressBar(getCurrentTimeFloat(currentMusic));
+  }, 50);
 
   currentMusic.addEventListener('pause', () => {
     clearInterval(plusOneSec);
+    clearInterval(plusFiftyMSec);
+
+    if (currentMusic.ended) handlePlayButton(false);
   });
 };
 
@@ -49,15 +74,14 @@ const togglePlay = () => {
   if (currentMusic) {
     if (currentMusic.paused) {
       currentMusic.play();
-      playBtn.classList.add('d-none');
-      pauseBtn.classList.remove('d-none');
+      handlePlayButton(true);
+      playerProgressHandler(currentMusic);
     } else {
       currentMusic.pause();
-      playBtn.classList.remove('d-none');
-      pauseBtn.classList.add('d-none');
+      handlePlayButton(false);
     }
 
-    playerProgressHandler(currentMusic);
+    // playerProgressHandler(currentMusic);
   }
 };
 
