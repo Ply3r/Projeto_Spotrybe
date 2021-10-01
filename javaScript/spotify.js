d975a72fe4a84b3189b1ba87c78124145dc5b9f1
Effolex <Effolex@gmail.com>
2021-10-01 16:20:54 -0300

    Update spotify.js

--- javaScript/spotify.js
+++ javaScript/spotify.js
@@ -9,9 +9,10 @@ class Spotify {
   }
 
   static async getToken() {
-
-    const CLIENT_ID = '062ce822e4104fa4827a8db0ee93263d';
-    const CLIENT_SECRET = 'e2e8aa8221984bb9959a7e2ef62de1e1';
+    const usuarioJSON = localStorage.getItem('currentUser');
+    const usuario = JSON.parse(usuarioJSON)
+    const CLIENT_ID = usuario.clientId;
+    const CLIENT_SECRET = usuario.clientSecret;
     const API_TOKEN = 'https://accounts.spotify.com/api/token';
     const response = await fetch(API_TOKEN, {
       body: 'grant_type=client_credentials',
